ddc0721ad9bd8eb64383b9f627423ce64b9fa2ab
Chris Schmalz <abjectfilm@gmail.com>
2016-03-12 00:38:54 -0800

    updated how it exits the program

--- learn/learn.cpp
+++ learn/learn.cpp
@@ -7,7 +7,7 @@
 int main()
 {
 	//init vars
-	int a,b,sum;
+	int a, b, sum, exit;
 
 	//ask for values
 	cout << "I'm a little computer brain that can add up two numbers. What's the first number you want me to add up?\n";
@@ -20,6 +20,8 @@ int main()
 
 	//print to console
 	cout << "\nHmm...thinking. \n\n13? 48? 1,433,456? \n\nNope. The sum must be " << sum << ".\n\n\n";
+	cout << "Type any number and hit the enter key to exit...\n";
+	cin >> exit;
 
 	//exit
     return 0;
