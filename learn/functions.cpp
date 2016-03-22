56079b954c9e16794a91c8b639f43815330486c4
Chris Schmalz <abjectfilm@gmail.com>
2016-03-21 21:57:25 -0700

    refreshed

---
+++ learn/functions.cpp
@@ -0,0 +1,12 @@
+
+//headers
+#include "stdafx.h"
+#include "functions.h"
+
+// add function
+int addNum(int x, int y) {
+	//add both numbers
+	int solution = x + y;
+	//return value
+	return solution;
+}
